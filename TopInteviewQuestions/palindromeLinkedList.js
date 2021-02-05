8483f1b50550d7c04a91dd4a366c204208a7d946
Siddharth Roy <siddharthroy36912@gmail.com>
2021-02-05 08:27:14 +0530

    New Submission

---
+++ TopInteviewQuestions/palindromeLinkedList.js
@@ -0,0 +1,47 @@
+// Palindrome Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let fast = head
+    let slow = head
+    
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next
+        slow = slow.next
+    }
+    
+    let prev = null
+    
+    while (slow !== null) {
+        let tmp = slow.next
+        slow.next = prev
+        prev = slow
+        slow = tmp
+    }
+    
+    let left = head
+    let right = prev
+    
+    while (right !== null) {
+        if (left.val !== right.val) {
+            return false
+        }
+        
+        left = left.next
+        right = right.next
+        
+    }
+    
+    return true
+    
+    
+};
